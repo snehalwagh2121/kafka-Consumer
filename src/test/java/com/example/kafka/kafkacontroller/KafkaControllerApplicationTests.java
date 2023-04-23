b407b14ed4176d7a75f592deb8055c78d3f97312
Harley <37466168+snehalwagh2121@users.noreply.github.com>
2023-04-23 17:42:02 +0530

    SPRING-51 - init

---
+++ src/test/java/com/example/kafka/kafkacontroller/KafkaControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka.kafkacontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
